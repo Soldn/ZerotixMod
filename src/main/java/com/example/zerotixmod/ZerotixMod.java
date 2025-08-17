ff7c71f728c1e16175c9ebd6ec06c599a2b55441
Soldn <kyryll524@gmail.com>
2025-08-17 13:24:07 +0100

    Add files via upload

---
+++ src/main/java/com/example/zerotixmod/ZerotixMod.java
@@ -0,0 +1,10 @@
+package com.example.zerotixmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("zerotixmod")
+public class ZerotixMod {
+    public ZerotixMod() {
+        System.out.println("ZerotixMod loaded!");
+    }
+}
