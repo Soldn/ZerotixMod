fbaf0ac91d931611a3bdf4f0aaf9c6909bb21f7e
Soldn <kyryll524@gmail.com>
2025-08-17 13:36:30 +0100

    Update ZerotixMod.java

--- src/main/java/com/zerotix/ZerotixMod.java
+++ src/main/java/com/zerotix/ZerotixMod.java
@@ -1,50 +1,25 @@
-package com.zerotix;
+package com.zerotixmod;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.GuiOpenEvent;
-import net.minecraftforge.client.event.GuiScreenEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.MainMenuScreen;
-import net.minecraft.client.gui.screen.DisconnectedScreen;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
-@Mod("zerotix")
-@EventBusSubscriber(modid = "zerotix", bus = Bus.FORGE, value = Dist.CLIENT)
+@Mod("zerotixmod")
 public class ZerotixMod {
+    public static final String MODID = "zerotixmod";
 
     public ZerotixMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-    }
-
-    private void clientSetup(final FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            try {
-                Minecraft mc = Minecraft.getInstance();
-                mc.getWindow().setTitle("Zerotix");
-            } catch (Throwable t) {
-                // ignore if not ready
-            }
-        });
+        // Конструктор мода
     }
 
     @SubscribeEvent
-    public static void onInitMain(GuiScreenEvent.InitGuiEvent.Post e) {
-        if (e.getGui() instanceof MainMenuScreen) {
-            Minecraft.getInstance().setScreen(new com.zerotix.client.ZerotixMainMenuScreen());
-        }
+    public void setup(FMLCommonSetupEvent event) {
+        System.out.println(">>> ZerotixMod загружен на сервере!");
     }
 
     @SubscribeEvent
-    public static void onOpenGui(GuiOpenEvent e) {
-        if (e.getGui() instanceof DisconnectedScreen) {
-            // wrap to our custom screen
-            DisconnectedScreen d = (DisconnectedScreen) e.getGui();
-            e.setGui(new com.zerotix.client.ZerotixDisconnectedScreen(d));
-        }
+    public void doClientStuff(FMLClientSetupEvent event) {
+        System.out.println(">>> ZerotixMod загружен на клиенте!");
     }
 }
